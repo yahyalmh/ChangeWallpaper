4b087713b9ab953d200e5a56ca50f7abab8f4841
Bina Co <binaco1400@gmail.com>
2021-09-03 02:51:22 +0430

    Update changeWall bash to set the wallpaper for cinnamon correctly

--- bash/changeWall.sh
+++ bash/changeWall.sh
@@ -1,52 +1,65 @@
 #!/usr/bin/env bash
 
+# echo "XDG_CURRENT_DESKTOP: $XDG_CURRENT_DESKTOP, BASH_VERSION: ${BASH_VERSION},  GDMSESSION: $GDMSESSION"
 imageAddress="$1"
 user=$(whoami)
 
 function is_cron_run() {
+  is_cron=0
+  if [[ -z "$PS1" ]]; then
+    is_cron=1
+  else
     is_cron=0
-    if [[ -z "$PS1" ]]; then
-        is_cron=1
-    else
-        is_cron=0
-    fi
-    return ${is_cron}
+  fi
+  return ${is_cron}
 }
 
 function export_dbus_first_way() {
-    pid_gnome=$(pgrep gnome-session -n -U $UID)
+  PID=$(pgrep gnome-session -n -U $UID)
 
-    DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/${pid_gnome}/environ| tr '\0' '\n'|cut -d= -f2-)
-    export DBUS_SESSION_BUS_ADDRESS=${DBUS_SESSION_BUS_ADDRESS}
+  if [[ -z "${PID}" ]]; then
+    PID=$(pgrep -o "cinnamon-sess|gnome-sess|mate-sess")
+  fi
+
+  dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/"${PID}"/environ | tr '\0' '\n' | cut -d= -f2-)
+  export DBUS_SESSION_BUS_ADDRESS=${dbus}
 }
+
 function export_dbus_second_way() {
-    file=$(find /proc -maxdepth 2 -user ${user} -name environ -print -quit)
+  file=$(find /proc -maxdepth 2 -user ${user} -name environ -print -quit)
+  per_file=${file}
+
+  for i in {1..20}; do
+    if [[ ! -f ${file} ]]; then
+      break
+    fi
     per_file=${file}
+    file=$(find /proc -maxdepth 2 -user ${user} -name environ -newer "${file}" -print -quit)
+  done
 
-    for i in {1..20}
-    do
-        if [[ ! -f ${file} ]] ; then
-             break
-        fi
-        per_file=${file}
-        file=$(find /proc -maxdepth 2 -user ${user} -name environ -newer "${file}" -print -quit)
-    done
-
-    export DBUS_SESSION_BUS_ADDRESS=$(grep -z DBUS_SESSION_BUS_ADDRESS "${per_file}" | tr '\0' '\n'| cut -d= -f2-)
+  dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS "${per_file}" | tr '\0' '\n' | cut -d= -f2-)
+  export DBUS_SESSION_BUS_ADDRESS=$dbus
 }
+
 function change_background() {
-    image_address="${1}"
+  image_address="${1}"
+  if [[ "$GDMSESSION" =~ ^ubuntu.* ]]; then
     gsettings set org.gnome.desktop.background picture-uri "file://${image_address}"
+  fi
+
+  if [[ "$GDMSESSION" =~ ^cinnamon.* ]]; then
+    gsettings set org.cinnamon.desktop.background picture-uri "file://${image_address}"
+  fi
 }
 
 is_cron_run
 is_cron=$?
 
-if [[ "$is_cron"="1" ]] ; then
-    export DISPLAY=:0
-    export GSETTINGS_BACKEND=dconf
-    export_dbus_second_way
-    export_dbus_first_way
+if [[ "$is_cron" == "1" ]]; then
+  export DISPLAY=:0
+  export GSETTINGS_BACKEND=dconf
+  export_dbus_second_way
+  export_dbus_first_way
 fi
 
 change_background "${imageAddress}"
