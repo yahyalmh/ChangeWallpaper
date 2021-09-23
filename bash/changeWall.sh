ae586a83d3846dad56bd701345bcb7eeabf4f1b9
yahyalmh <yahyamh01@gmail.com>
2021-09-23 19:45:12 +0330

    Fix change background problem, GDMSESSION was empty for a bash that is run from crontab

--- bash/changeWall.sh
+++ bash/changeWall.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# echo "XDG_CURRENT_DESKTOP: $XDG_CURRENT_DESKTOP, BASH_VERSION: ${BASH_VERSION},  GDMSESSION: $GDMSESSION"
 imageAddress="$1"
 user=$(whoami)
 
@@ -18,7 +17,7 @@ function export_dbus_first_way() {
   PID=$(pgrep gnome-session -n -U $UID)
 
   if [[ -z "${PID}" ]]; then
-    PID=$(pgrep -o "cinnamon-sess|gnome-sess|mate-sess")
+    PID=$(pgrep -o "cinnamon-sess|gnome-sess|mate-sess|xfce4-sess|kded4-sess|kded5-sess")
   fi
 
   dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS /proc/"${PID}"/environ | tr '\0' '\n' | cut -d= -f2-)
@@ -26,15 +25,16 @@ function export_dbus_first_way() {
 }
 
 function export_dbus_second_way() {
-  file=$(find /proc -maxdepth 2 -user ${user} -name environ -print -quit)
+  file=$(find /proc -maxdepth 2 -user "${user}" -name environ -print -quit)
   per_file=${file}
 
+  # shellcheck disable=SC2034
   for i in {1..20}; do
     if [[ ! -f ${file} ]]; then
       break
     fi
     per_file=${file}
-    file=$(find /proc -maxdepth 2 -user ${user} -name environ -newer "${file}" -print -quit)
+    file=$(find /proc -maxdepth 2 -user "${user}" -name environ -newer "${file}" -print -quit)
   done
 
   dbus=$(grep -z DBUS_SESSION_BUS_ADDRESS "${per_file}" | tr '\0' '\n' | cut -d= -f2-)
@@ -43,13 +43,10 @@ function export_dbus_second_way() {
 
 function change_background() {
   image_address="${1}"
-  if [[ "$GDMSESSION" =~ ^ubuntu.* ]]; then
-    gsettings set org.gnome.desktop.background picture-uri "file://${image_address}"
-  fi
 
-  if [[ "$GDMSESSION" =~ ^cinnamon.* ]]; then
-    gsettings set org.cinnamon.desktop.background picture-uri "file://${image_address}"
-  fi
+  # changing background in desktop environments is different, for now this two ways uses, I try to use both of the to be sure.
+  gsettings set org.cinnamon.desktop.background picture-uri "file://${image_address}" 1>/dev/null 2>/dev/null
+  gsettings set org.gnome.desktop.background picture-uri "file://${image_address}" 1>/dev/null 2>/dev/null
 }
 
 is_cron_run
