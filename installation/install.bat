76277f4e89ea40c23bbbdd20a83f2ce0cc197368
yahyalmh <yahyamh01@gmail.com>
2021-09-24 22:56:16 +0330

    Spell checking

--- installation/install.bat
+++ installation/install.bat
@@ -7,23 +7,24 @@ set run_path=%private_dir_path%\run.vbs
 
 for /f "delims=" %%i in ('where python3') do set python3_path=%%i
 if not %python3_path%=="" (
-	echo "you have installed python3 in %python3_path%"
+	echo "You have installed python3 in %python3_path%"
 
-	echo "Info: create app directory in path: %private_dir_path%"
+	echo "Info: Creating app directory in path: %private_dir_path%"
 
 	mkdir %private_dir_path%
 
-	echo "Info: coping files..."
+	echo "Info: Coping files..."
 	xcopy /r /d /i /s /y /exclude:excludelist.txt ..\* %private_dir_path%
 
-	echo "run path is:  %run_path%"
-    echo "Info: run app..."
+	echo "Run path is:  %run_path%"
+    echo "Info: Run app..."
     cmd /c %run_path%
 
-    echo "Info: create crontab job to change wallpaper hourly(see crontab file)"
-    echo "Info: download Bing and Nasa wallpaper if net connection was ok"
+    echo "Info: Create schtasks job to change wallpaper hourly(see \"schtasks /query /fo list\" command)"
+    echo "Info: See \"schtasks /query /fo list\" command"
+    echo "Info: Downloading Bing and Nasa wallpaper if the internet was reachable"
     echo "Info: Finished"
 
 	) else (
-	echo "Error: you have not installed python3. This app work with python3 only."
+	echo "Error: you have not installed python3. This app works only with python3."
 	)
--- installation/install.sh
+++ installation/install.sh
@@ -3,22 +3,23 @@
 is_installed=$(type -P python3  >/dev/null 2>&1 && echo 1 )
 
 if [ "$is_installed" = 1 ]; then
-    echo "Info: you have install python3"
+    echo "Info: You have installed python3"
     private_dir_name=".changeWall"
     private_dir_path="$HOME/${private_dir_name}"
-    echo "Info: create app directory in path:'$private_dir_path'"
+    echo "Info: creating app directory in path:'$private_dir_path'"
     mkdir -p "${private_dir_path}"
 
-    echo "Info: coping files..."
+    echo "Info: Coping files..."
     rsync -a ../* "$private_dir_path" --exclude install.sh
 
     python3_path=$(type -P python3)
     main_path="$HOME/${private_dir_name}/Main.py"
-    echo "Info: run app..."
+    echo "Info: Run app..."
     $python3_path "$main_path"
-    echo "Info: create crontab job to change wallpaper hourly(see crontab file)"
-    echo "Info: download Bing and Nasa wallpaper if net connection was ok"
+    echo "Info: Create crontab job to change wallpaper hourly and when reboot"
+    echo "Info: See crontab file by \"crontab -l\" command"
+    echo "Info: Downloading Bing and Nasa wallpaper if the internet was reachable"
     echo "Info: Finished"
 
-else echo "Error: you have not installed python3. This app work with python3 only."
+else echo "Error: you have not installed python3. This app works only with python3."
 fi
